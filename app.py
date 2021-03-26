3d55eb330a2c5caeabc6416f6ecc04a99a9aaf93
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-03-26 19:16:40 +0000

    added style

--- app.py
+++ app.py
@@ -23,6 +23,7 @@ def get_tasks():
     tasks = mongo.db.tasks.find()
     return render_template("tasks.html", tasks=tasks)
 
+
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
         port=int(os.environ.get("PORT")),
