8a0bf2b87383fab8d4d6a54b5906a9afda2f491e
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-03-31 14:31:06 +0000

    updated requirements

--- app.py
+++ app.py
@@ -15,6 +15,7 @@ app.config["MONGO_DBNAME"] = os.environ.get("MONGO_DBNAME")
 app.config["MONGO_URI"] = os.environ.get("MONGO_URI")
 app.secret_key = os.environ.get("SECRET_KEY")
 
+
 mongo = PyMongo(app)
 
 
@@ -22,7 +23,7 @@ mongo = PyMongo(app)
 @app.route("/get_tasks")
 def get_tasks():
     tasks = mongo.db.tasks.find()
-    return render_template("tasks.html", tasks=tasKs)
+    return render_template("tasks.html", tasks=tasks)
     
 
 @app.route("/register", methods=["GET", "POST"])
