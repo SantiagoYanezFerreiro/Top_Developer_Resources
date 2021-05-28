ebbf0296adcd585b9a8ac5336cd06e6e23ae92f0
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-05-28 20:25:23 +0000

    added emailjs functionality to make form work

---
+++ static/js/email.js
@@ -0,0 +1,21 @@
+//use to receive the info submitted on the form in my personal email using EmailJS API
+
+function sendForm(contactForm) {
+    emailjs.send("service_zbsju9a", "topdevelopmenttemplate", {
+      "from_name": contactForm.name.value,
+      "from_email": contactForm.email.value,
+      "message": contactForm.request.value
+    })
+      //if the information is succesfully submited an alert will be displayed 
+      .then(
+        function (response) {
+          console.log("form succesfully submited", response);
+          alert('Thank you for contacting us, we will get back to you soon!');
+          window.location.href="https://8080-jade-hawk-h4llo5iu.ws-eu08.gitpod.io/";
+        },
+        function (error) {
+          console.log("Sth weng wrong, please try again", error);
+        },
+    );
+    return false; // To prevent a new page from loading
+  }
