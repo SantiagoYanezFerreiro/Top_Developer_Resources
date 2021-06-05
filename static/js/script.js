1dad50a9693bc0cf5951e922e3a6a61d18bba17f
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-06-05 13:19:31 +0000

    added resources js functionalities

--- static/js/script.js
+++ static/js/script.js
@@ -1,1 +1,27 @@
+$(document).ready(function () {
+     // Hide Resources in the Profile Page
+    $('#user_resources').hide();
+    $('#hide_resources_btn').hide();
+    // Counter
+    $('.count-up').counter();
+    $('.count1').counter();
+    $('.count2').counter();
+    // bootstrap tooltip functionality
+    $('[data-toggle="tooltip"]').tooltip(); 
+});
+
+ // Hide Resources in the Profile Page
+ $('#hide_resources_btn').click(function(){
+    $('#hide_resources_btn').hide();
+    $('#show_resources_btn').show();
+    $('#user_resources').hide(); 
+});
+
+ // show Resources in the Profile Page
+$('#show_resources_btn').click(function(){
+    $('#show_resources_btn').hide();
+    $('#hide_resources_btn').show();
+    $('#user_resources').show(); 
+});
+
 
