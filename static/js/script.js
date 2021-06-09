3adbd4b8d29d975246abb2bc559ed9188fbaa9ae
SantiagoYanezFerreiro <santiagoyanez1990@gmail.com>
2021-06-09 18:07:16 +0000

    simplified js code

--- static/js/script.js
+++ static/js/script.js
@@ -3,21 +3,21 @@ $(document).ready(function () {
     $('#user_resources').hide();
     $('#hide_resources_btn').hide();
     // bootstrap tooltip functionality
-    $('[data-toggle="tooltip"]').tooltip(); 
-});
-
- // Hide Resources in the Profile Page
+    $('[data-toggle="tooltip"]').tooltip();
+    // Hide Resources in the Profile Page
  $('#hide_resources_btn').click(function(){
     $('#hide_resources_btn').hide();
     $('#show_resources_btn').show();
     $('#user_resources').hide(); 
 });
-
  // show Resources in the Profile Page
 $('#show_resources_btn').click(function(){
     $('#show_resources_btn').hide();
     $('#hide_resources_btn').show();
     $('#user_resources').show(); 
+}); 
 });
+
+ 
 
 
